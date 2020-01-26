0bef2e5688aabd3054be614e3273c93b7d6f8964
chromeoverload <chromeoverload@gmail.com>
2020-01-26 17:36:38 -0500

    test page edits

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Welcome to Null Traders!");
+        primaryStage.setScene(new Scene(root, 1600, 900));
         primaryStage.show();
     }
 
