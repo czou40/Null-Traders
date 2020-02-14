6a9ed829212e2c4d3820ca652389c56c8b276e0d
chromeoverload <chromeoverload@gmail.com>
2020-02-13 23:27:33 -0500

    Revert "Music Implementation"
    This reverts commit 25863d8ba4f0ec27875f6b7d6be830ed701c630b.

--- src/GameScreen.java
+++ src/GameScreen.java
@@ -5,17 +5,9 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
-import java.io.File;
-
 public abstract class GameScreen extends Screen {
-    private static Media bkg;
-    static MediaPlayer bkgPlayer;
-    private static boolean hasStarted;
-
     private String title;
     private boolean requiresSideBar;
     private Pane contentPane;
@@ -23,11 +15,6 @@ public abstract class GameScreen extends Screen {
         super(primaryStage, game);
         this.title = title.toUpperCase();
         this.requiresSideBar = requiresSideBar;
-
-        //music
-        bkg = new Media(new File("out/production/cs2340/sounds/game.mp3").toURI().toString());
-        bkgPlayer = new MediaPlayer(bkg);
-        bkgPlayer.setCycleCount(MediaPlayer.INDEFINITE);
     }
 
     public ReadOnlyDoubleProperty getContentWidth() {
@@ -40,13 +27,6 @@ public abstract class GameScreen extends Screen {
 
     @Override
     public Scene constructScene() {
-        //music stuff
-        if(!hasStarted) {
-            bkgPlayer.play();
-            WelcomeScreen.themePlayer.stop();
-            hasStarted = true;
-        }
-
         Label titleLabel = new Label(title);
         titleLabel.getStyleClass().add("h1");
         MyGridPane titlePane = new MyGridPane();
